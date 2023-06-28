c4498391780443ed80d192d247c3f3c392042508
Mohit Sharma <mohit7575@gmail.com>
2023-06-28 13:42:50 +0100

    updated Kotlin driver dependency.

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,12 +8,11 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
-    maven(url = "https://oss.sonatype.org/content/repositories/snapshots")
 }
 
 dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
-    implementation("org.mongodb:mongodb-driver-kotlin-coroutine:4.10.0-SNAPSHOT")
+    implementation("org.mongodb:mongodb-driver-kotlin-coroutine:4.10.1")
     implementation("org.slf4j:slf4j-simple:1.7.9")
 
     testImplementation("org.jetbrains.kotlin:kotlin-test")
